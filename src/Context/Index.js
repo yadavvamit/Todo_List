b0c4e77e88587c812f724b4e0208e7d0ca75276e
yadavvamit <yadavvamit223222@gmail.com>
2023-10-22 00:19:25 +0530

    first commit

---
+++ src/Context/Index.js
@@ -0,0 +1,7 @@
+import { TodoContext } from "./TodoContext";
+
+import { TodoProvider } from "./TodoContext";
+
+import { useTodo } from "./TodoContext";
+
+export {TodoContext, TodoProvider, useTodo}
